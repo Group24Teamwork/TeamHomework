46f6c7eddd1fa54a8f3039a911337c1fccb795aa
bozhidarbozhilov <bozhidarbozhilov@gmail.com>
2018-03-12 18:03:57 +0200

    Added problem 2 solution by Bozhidar

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # TeamHomework
 Group24`s homework
 1. Georgi added solution for problem 1 "Debit Card"!
+2. Bozhidar added solution for p02. Rectangle Area.
---
+++ intro-and-basic-syntax/exercise/RectangleArea.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace p02._RectangleArea
+{
+    class RectangleArea
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{a*b:F2}");
+        }
+    }
+}
