7a97c4c489a16bfa77ca0258fac03dce2670d4a7
Nikoleta <nikoleta18@abv.bg>
2018-03-21 23:19:55 +0200

    Change variables

--- intro-and-basic-syntax/exercise/RectangleArea.cs
+++ intro-and-basic-syntax/exercise/RectangleArea.cs
@@ -7,11 +7,11 @@ namespace p02._RectangleArea
         static void Main(string[] args)
         {
             //In the next 2 lines we receive from the Console the height and the length of the rectangle
-            double a = double.Parse(Console.ReadLine());
-            double b = double.Parse(Console.ReadLine());
+            double length = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
 
             //Here we print the area of the rectangle rounding to the second decimal point
-            Console.WriteLine($"{a*b:F2}");
+            Console.WriteLine($"{length*height:F2}");
         }
     }
 }
