010c7457ea9c635596ebf1208baad9abd9aaffa5
chokokn <37249562+chokokn@users.noreply.github.com>
2018-03-22 21:32:01 +0200

    Nikolay added solution for problem 3 "Miles to Kilometers"!

---
+++ intro-and-basic-syntax/exercise/MilesToKilometers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _03._Miles_to_Kilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var miles = double.Parse(Console.ReadLine());
+
+            var km = miles * 1.60934;
+
+            Console.WriteLine($"{km:F2}");
+        }
+    }
+}
