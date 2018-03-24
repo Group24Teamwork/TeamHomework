d8cf97470babed0ef36cf87b3a0bcdea0d7aa6a7
bozhidarbozhilov <bozhidarbozhilov@gmail.com>
2018-03-24 07:58:09 +0200

    Bozhidar documented p01. DebitCard.cs

--- intro-and-basic-syntax/exercise/DebitCard.cs
+++ intro-and-basic-syntax/exercise/DebitCard.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 
 namespace DebitCard
 {
@@ -6,11 +7,13 @@ namespace DebitCard
     {
         static void Main(string[] args)
         {
+			//Receive four random integer numbers - max length: 4 digits.
             var firstNumber = int.Parse(Console.ReadLine());
             var secondNumber = int.Parse(Console.ReadLine());
             var thirdNumber = int.Parse(Console.ReadLine());
             var fourthNumber = int.Parse(Console.ReadLine());
 
+			//Print the entered four numbers in debit card format
             Console.WriteLine("{0:D4} {1:D4} {2:D4} {3:D4}", firstNumber, secondNumber, thirdNumber, fourthNumber);
         }
     }
