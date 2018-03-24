6c907e1f3aa894599ec3641c8ad99a1b26d211ec
bozhidarbozhilov <bozhidarbozhilov@gmail.com>
2018-03-24 07:52:55 +0200

    Bozhidar (as fifth member) add solution for p5. CharacterStats

---
+++ intro-and-basic-syntax/exercise/CharacterStats.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace p05._Character_Stats
+{
+    class CharacterStats
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int currentHealth = int.Parse(Console.ReadLine());
+            int maxHealth = int.Parse(Console.ReadLine());
+            int currentEnergy = int.Parse(Console.ReadLine());
+            int maxEnergy = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}");
+            Console.Write("Health: |");
+            Console.Write(new string('|', currentHealth));
+            Console.Write(new string('.', maxHealth - currentHealth));
+            Console.WriteLine("|");
+
+            Console.Write("Energy: |");
+            Console.Write(new string('|', currentEnergy));
+            Console.Write(new string('.', maxEnergy - currentEnergy));
+            Console.WriteLine("|");
+        }
+    }
+}
