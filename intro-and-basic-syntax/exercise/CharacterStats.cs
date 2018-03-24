d0daac66f9c7d240650e4610ea1b7b7505db5c52
Nikoleta <nikoleta18@abv.bg>
2018-03-24 13:36:09 +0200

    Add comments for problem 5

--- intro-and-basic-syntax/exercise/CharacterStats.cs
+++ intro-and-basic-syntax/exercise/CharacterStats.cs
@@ -6,18 +6,25 @@ namespace p05._Character_Stats
     {
         static void Main(string[] args)
         {
+			//Read hero name from console
             string heroName = Console.ReadLine();
+			
+			//Read and parse hero stats
             int currentHealth = int.Parse(Console.ReadLine());
             int maxHealth = int.Parse(Console.ReadLine());
             int currentEnergy = int.Parse(Console.ReadLine());
             int maxEnergy = int.Parse(Console.ReadLine());
 
+			//Display hero name
             Console.WriteLine($"Name: {heroName}");
+			
+			//Output health to console
             Console.Write("Health: |");
             Console.Write(new string('|', currentHealth));
             Console.Write(new string('.', maxHealth - currentHealth));
             Console.WriteLine("|");
-
+			
+			//Output energy to console
             Console.Write("Energy: |");
             Console.Write(new string('|', currentEnergy));
             Console.Write(new string('.', maxEnergy - currentEnergy));
